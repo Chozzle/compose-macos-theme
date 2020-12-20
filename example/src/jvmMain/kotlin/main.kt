2b11f7619d0027071b23a27cdd8efa1bf8b74583
Carson Holzheimer <carson@redeye.co>
2020-12-21 09:59:02 +1000

    Start dropdown

--- example/src/jvmMain/kotlin/main.kt
+++ example/src/jvmMain/kotlin/main.kt
@@ -26,66 +26,73 @@ import me.carsonholzheimer.composemacostheme.modifiedofficial.MacOutlinedTextFie
 
 fun main() = Window {
 
-    ExampleView()
+    MacTheme {
+        Column {
+            ExampleView()
 
-    /*DropdownMenu(
-        toggle = {
-            Row(
-                modifier = Modifier.clickable(onClick = { showMenu = true }),
+            val items = listOf("Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet")
+            var showMenu by remember { mutableStateOf(false) }
+            var selectedIndex by remember { mutableStateOf(0) }
+            DropdownMenu(
+                toggle = {
+                    Row(
+                        modifier = Modifier.clickable(onClick = { showMenu = true }),
 
-                ) {
-                Text(
-                    items[selectedIndex],
-                    modifier = Modifier.padding(2.dp),
-                    style = TextStyle(fontSize = 13.sp)
-                )
-                Surface(
-                    modifier = Modifier.size(16.dp),
-                    color = MaterialTheme.colors.primary,
-                    shape = MaterialTheme.shapes.small
-                ) {
-                    Text(
-                        "\uDBC0\uDD87\uDBC0\uDD88",
-                        fontSize = 8.sp,
-                        fontWeight = W700,
-                        lineHeight = 6.sp,
-                        textAlign = TextAlign.Center
-                    )
+                        ) {
+                        Text(
+                            items[selectedIndex],
+                            modifier = Modifier.padding(2.dp),
+                            style = TextStyle(fontSize = 13.sp)
+                        )
+                        Surface(
+                            modifier = Modifier.size(16.dp),
+                            color = MaterialTheme.colors.primary,
+                            shape = MaterialTheme.shapes.small
+                        ) {
+                            Text(
+                                "\uDBC0\uDD87\uDBC0\uDD88",
+                                fontSize = 8.sp,
+                                fontWeight = W700,
+                                lineHeight = 6.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                },
+                expanded = showMenu,
+                onDismissRequest = { showMenu = false },
+                Modifier.border(1.dp, MacTheme.colors.border, RoundedCornerShape(4.dp)),
+                dropdownOffset = Position((-100).dp, (-20).dp) // TODO get exact widths
+            ) {
+                items.forEachIndexed { index, string ->
+                    var isMouseHovering by remember { mutableStateOf(false) }
+                    DropdownMenuItem(
+                        onClick = {
+                            selectedIndex = index
+                            showMenu = false
+                        },
+                        modifier = Modifier.background(if (isMouseHovering) MacTheme.colors.highlight else Color.Unspecified)
+                            .height(20.dp)
+                            .pointerMoveFilter(
+                                onEnter = {
+                                    isMouseHovering = true
+                                    false
+                                },
+                                onExit = {
+                                    isMouseHovering = false
+                                    false
+                                }
+                            )
+                    ) {
+                        Text(
+                            text = (if (index == selectedIndex) "􀆅 " else "     ") + string,
+                            color = if (isMouseHovering) Color.White else Color.Unspecified,
+                            style = TextStyle(fontSize = 13.sp)
+
+                        )
+                    }
                 }
             }
-        },
-        expanded = showMenu,
-        onDismissRequest = { showMenu = false },
-        Modifier.border(1.dp, MacTheme.colors.border, RoundedCornerShape(4.dp)),
-        dropdownOffset = Position((-100).dp, (-20).dp) // TODO get exact widths
-    ) {
-        items.forEachIndexed { index, string ->
-            var isMouseHovering by remember { mutableStateOf(false) }
-            DropdownMenuItem(
-                onClick = {
-                    selectedIndex = index
-                    showMenu = false
-                },
-                modifier = Modifier.background(if (isMouseHovering) MacTheme.colors.highlight else Color.Unspecified)
-                    .height(20.dp)
-                    .pointerMoveFilter(
-                        onEnter = {
-                            isMouseHovering = true
-                            false
-                        },
-                        onExit = {
-                            isMouseHovering = false
-                            false
-                        }
-                    )
-            ) {
-                Text(
-                    text = (if (index == selectedIndex) "􀆅 " else "     ") + string,
-                    color = if (isMouseHovering) Color.White else Color.Unspecified,
-                    style = TextStyle(fontSize = 13.sp)
-
-                )
-            }
         }
-    }*/
+    }
 }
